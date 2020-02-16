1c565ba639748dda0c99e28ae5e4ddf7ec16c1e3
Frank Gao <frank@swoag.com>
2020-02-16 04:59:59 -0600

    removed extra files

--- FlaskApp/config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = '967680C58CA344B8601246255559AE06B4C1B09B9DB0DCC4'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- FlaskApp/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField('stu', validators=[DataRequired()])
-    password = PasswordField('f', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Submit')
